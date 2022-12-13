345df90d1883f81a3a1be639506bf8fc8d46a83e
Jason Stajich <jasonstajich.phd@gmail.com>
2022-12-12 18:38:53 -0800

    sync data

--- make_featurefile.sh
+++ make_featurefile.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash -l
 module load bcftools
 module load genometools
-DATFILE=BcinereaB05-10.features.gff3
-FINAL=BcinereaB05-10.features.sort.gff3
-cat genomes/botrytis/FungiDB-60_BcinereaB05-10.gff TE/BcinereaB05-10.RM.gff3 | grep -v -P "\t(gene|mRNA|CDS|three_prime_UTR|five_prime_UTR|protein_coding_gene|ncRNA_gene)\t"  > $DATFILE
-gt gff3 -addintrons genomes/botrytis/FungiDB-60_BcinereaB05-10.gff  | grep intron >> $DATFILE
+DIR=genomes/botrytis
+IN=$DIR/FungiDB-60_BcinereaB05-10.gff
+TE=TE/BcinereaB05-10.RM.gff3
+DATFILE=$DIR/BcinereaB05-10.features.gff3
+FINAL=$DIR/BcinereaB05-10.features.sort.gff3
+cat $IN $TE | grep -v -P "\t(gene|mRNA|CDS|three_prime_UTR|five_prime_UTR|protein_coding_gene|ncRNA_gene)\t"  > $DATFILE
+gt gff3 -addintrons $IN | grep -P "\tintron\t" >> $DATFILE
 (grep ^"#" $DATFILE; grep -v ^"#" $DATFILE | grep -v "^$" | grep "\t" | sort -k1,1 -k4,4n) > $FINAL
-
 bgzip -f $FINAL
 tabix $FINAL.gz
